3dfa1c6cc50d601389dbb1e97d12f159c73d69db
SirWilliamIII <willcarpenterwastaken@gmail.com>
2020-10-19 18:50:07 -0500

    fixed post body

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,23 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express')
+const router = express.Router()
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', {
-        title: 'Node Tutorial'
-    });
-});
+        title: 'Title'
+    })
+})
 
 router.post('/', function(req, res) {
-    var body = req.body;
+    const body = req.body
 
-    var res_body = {
+    const res_body = {
         first_name: body.first_name,
         last_name: body.last_name,
         email: body.email
-    };
+    }
 
-    res.render('welcome', res_body);
-});
+    res.render('welcome', res_body)
+})
 
-module.exports = router;
+module.exports = router
